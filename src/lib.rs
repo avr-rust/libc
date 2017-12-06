3f287f932d121ff11ae0365c7ef54b65f4a50367
Dylan McKay <me@dylanmckay.io>
2017-12-07 04:03:33 +1300

    Add crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! Rust bindings to [avr-libc](http://www.nongnu.org/avr-libc/).
+//!
+//! When compiled with the avr-rust compiler, the libc bindings in this crate
+//! automatically pick up the right mcu-specific headers depending on what
+//! cpu is specified in the target specification file.
+//!
+//! This means that on different microcontrollers, this crate will have a different
+//! public API. In general, most functions are the same across all chips. Many
+//! constants however are device-specific.
+
 #![no_std]
 
 #![allow(non_camel_case_types, non_upper_case_globals)]
